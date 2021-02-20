e96f89b755e30f1e5bbb31ce26ee367e11ec001a
yamamura-k <yamayama23bb@gmail.com>
2021-02-20 12:00:01 +0900

    output result

--- cuda_tutorial1/add_vector.cu
+++ cuda_tutorial1/add_vector.cu
@@ -7,17 +7,29 @@ __global__ void add_vector(float *out, float *a, float *b, int n)
     }
 }
 
-void main()
+int main()
 {
     const int N = 10000000;
     float *a, *b, *out;
     float *cuda_a;
 
     a = (float*)malloc(sizeof(float) * N);
+    b = (float*)malloc(sizeof(float) * N);
+    for( int i = 0 ; i < N ; i++ )
+    {
+        a[i] = i*1.1;
+        b[i] = N*0.6-i;
+    }
     cudaMalloc((void**)&cuda_a, sizeof(float) * N);
     cudaMemcpy(cuda_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
 
     add_vector<<<1,1>>>(out, cuda_a, b, N);
+    for( int i = 0 ; i < N ; ++i )
+    {
+      std::cout << out[i] << "  ";
+    }
+    std::cout << std::endl;
     cudaFree(cuda_a);
     free(a);
+    return 0;
 }
