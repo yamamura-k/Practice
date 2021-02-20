c00d14d4913f2cdb435f452a1d90caf7fd8a279b
yamamura-k <yamayama23bb@gmail.com>
2021-02-20 12:20:06 +0900

    add some variable

--- cuda_tutorial1/add_vector.cu
+++ cuda_tutorial1/add_vector.cu
@@ -9,9 +9,9 @@ __global__ void add_vector(float *out, float *a, float *b, int n)
 
 int main()
 {
-    const int N = 10000000;
+    const int N = 10;
     float *a, *b, *out;
-    float *cuda_a;
+    float *cuda_a, *cuda_b, *cuda_out;
 
     a = (float*)malloc(sizeof(float) * N);
     b = (float*)malloc(sizeof(float) * N);
@@ -23,9 +23,13 @@ int main()
         out[i] = 0.0;
     }
     cudaMalloc((void**)&cuda_a, sizeof(float) * N);
+    cudaMalloc((void**)&cuda_b, sizeof(float) * N);
+    cudaMalloc((void**)&cuda_out, sizeof(float) * N);
     cudaMemcpy(cuda_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(cuda_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
+    cudaMemcpy(cuda_out, out, sizeof(float) * N, cudaMemcpyHostToDevice);
 
-    add_vector<<<1,1>>>(out, cuda_a, b, N);
+    add_vector<<<1,1>>>(cuda_out, cuda_a, cuda_b, N);
     for( int i = 0 ; i < N ; ++i )
     {
       std::cout << out[i] << "  ";
